83d7b22ecc7ffdbcdc87ab7bbf90592ed4d6e0a3
gashiardit <arditfcb58@gmail.com>
2024-05-05 13:49:31 +0200

    changes on Homepage code

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,30 +1,19 @@
-// cypress/integration/home_page_spec.js
+describe('ParaBank Home Page', () => {
+  beforeEach(() => {
+    cy.visit('https://parabank.parasoft.com/parabank/index.htm')
+  })
 
-describe('Home Page Test', () => {
-  it('Loads the home page and verifies elements', () => {
-    // Visit the home page
-    cy.visit('https://parabank.parasoft.com/parabank/index.htm');
-
-    // Verify the presence of main elements
-    cy.get('#mainPanel').should('exist');
-    cy.get('#topPanel').should('exist');
-    cy.get('#headerPanel').should('exist');
-    cy.get('#bodyPanel').should('exist');
-    cy.get('#footermainPanel').should('exist');
-
-    // Verify the presence of specific elements
-    cy.get('h2').contains('Customer Login').should('exist');
-    cy.get('form[name="login"]').should('exist');
-    cy.get('form[name="login"] input[name="username"]').should('exist');
-    cy.get('form[name="login"] input[name="password"]').should('exist');
-    cy.get('form[name="login"] input[type="submit"]').should('exist');
-    cy.contains('Forgot login info?').should('exist');
-    cy.contains('Register').should('exist');
-
-    // Test login form functionality (You may need to modify this part based on your actual login process)
-    cy.get('form[name="login"] input[name="username"]').type('testuser');
-    cy.get('form[name="login"] input[name="password"]').type('password');
-    cy.get('form[name="login"]').submit();
-
-  });
-});
+  it('should display all required elements', () => {
+    cy.get('.logo').should('be.visible')
+    cy.get('#loginPanel').should('be.visible')
+    cy.get('input[name="username"]').should('be.visible')
+    cy.get('input[name="password"]').should('be.visible')
+    cy.get('input[type="submit"]').should('be.visible')
+    cy.contains('Forgot login info?').should('be.visible')
+    cy.contains('Register').should('be.visible')
+    cy.contains('ATM Services').should('be.visible')
+    cy.contains('Online Services').should('be.visible')
+    cy.contains('Latest News').should('be.visible')
+    cy.contains('Read More').should('be.visible')
+  })
+})
