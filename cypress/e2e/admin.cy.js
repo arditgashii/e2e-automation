7756138b902a562bc2668a188c1548403e37ff08
gashiardit <arditfcb58@gmail.com>
2024-05-05 11:30:56 +0200

    Updated Admin page code

--- cypress/e2e/admin.cy.js
+++ cypress/e2e/admin.cy.js
@@ -1,18 +1,27 @@
 describe('Admin Page Actions', () => {
-  it('MVP: Perform actions on Admin Page', () => {
+  it('Perform actions on Admin Page', () => {
       // Visit the Admin Page
       cy.visit('https://parabank.parasoft.com/parabank/admin.htm');
 
-      // Click on the SOAP radio button
-      cy.get('#accessMode4').check();
+      // Click the button to clean actions
+      cy.get('button.button[name="action"][value="CLEAN"]').click();
 
-      // Select an option from the Loan Provider dropdown
+      // Click on the JDBC* radio button and verify it is checked
+      cy.get('#accessMode4').check().should('be.checked');
+
+      // Enter '500' into the Initial Balance input
+      cy.get('#initialBalance').clear().type('1500');
+
+      // Enter '200' into the Minimum Balance input
+      cy.get('#minimumBalance').clear().type('450');
+
+      // Select 'Web Service' from the Loan Provider dropdown
       cy.get('#loanProvider').select('Web Service');
 
-      // Enter a value in the Threshold input
+      // Enter '30' into the Threshold input
       cy.get('#loanProcessorThreshold').clear().type('30');
 
-      // Click on the Submit button
+      // Submit the form
       cy.get('#adminForm').submit();
   });
 });
