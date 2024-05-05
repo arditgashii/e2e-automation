d9ade0242b2f4c2827a93cc2e390c12d17b9f6cb
gashiardit <arditfcb58@gmail.com>
2024-05-05 11:22:40 +0200

    Updated About Us page code

--- cypress/e2e/aboutUs.cy.js
+++ cypress/e2e/aboutUs.cy.js
@@ -3,46 +3,26 @@ describe('About Us Page Tests', () => {
     cy.visit('https://parabank.parasoft.com/parabank/about.htm');
   });
 
-
-  it('checks visibility of all necessary elements on the About Us page', () => {
+  it('confirms page load and checks visibility of main text', () => {
     // Confirm the page is loaded
-    cy.url().should('eq', 'https://parabank.parasoft.com/parabank/about.htm');
+    cy.url().should('eq', 'https://parabank.parasoft.com/parabank/about.htm').then(() => {
+      cy.log("Page loaded successfully!");
+    });
 
-    cy.get('body').should('be.visible');
+    // Check visibility of main text
     cy.contains('ParaSoft Demo Website').should('be.visible');
-    // Check for the visibility of the top panel elements
-    cy.get('.admin').should('be.visible');
-    cy.get('.logo').should('be.visible');
-    cy.get('.caption').should('be.visible');
+    cy.contains('ParaBank is a demo site').should('be.visible');
+    cy.contains('not a real bank!').should('be.visible');
+    cy.contains('For more information about Parasoft solutions').should('be.visible');
+    cy.contains('www.parasoft.com').should('be.visible');
 
-    // Verify header navigation links are visible
-    cy.get('#headerPanel ul.leftmenu li').each((item) => {
-      cy.wrap(item).should('be.visible');
-    });
-
-    // Verify the button links in the header
-    cy.get('#headerPanel ul.button li').each((item) => {
-      cy.wrap(item).should('be.visible');
-    });
-
-    // Check for the visibility of the main content elements
-    cy.get('h1.title').should('be.visible').and('contain', 'ParaSoft Demo Website');
-    cy.get('#rightPanel p').should('be.visible');
-
-    // Check for the login form visibility
-    cy.get('#loginPanel').should('be.visible');
+    // Check visibility of other elements in page
+    cy.get('h1').should('be.visible');
     cy.get('input[name="username"]').should('be.visible');
     cy.get('input[name="password"]').should('be.visible');
     cy.get('input[type="submit"]').should('be.visible');
-
-    // Footer checks
-    cy.get('#footerPanel').should('be.visible');
-    cy.get('#footerPanel ul li').each((item) => {
-      cy.wrap(item).should('be.visible');
-    });
-    cy.get('#footerPanel p.copyright').should('be.visible');
-    cy.get('ul.visit li').each((item) => {
-      cy.wrap(item).should('be.visible');
-    });
+    cy.get('.admin').should('be.visible');
+    cy.get('.logo').should('be.visible');
+    cy.get('a').should('be.visible');
   });
 });
