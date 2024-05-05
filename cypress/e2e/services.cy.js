568b9258720d692b0702cf82d9190d222292675c
gashiardit <arditfcb58@gmail.com>
2024-05-05 11:42:21 +0200

    Updated Services page code

--- cypress/e2e/services.cy.js
+++ cypress/e2e/services.cy.js
@@ -1,9 +1,16 @@
 describe('Testing Services page', () => {
-  it('MVP: Get the number of table rows', () => {
+  it('Get the number of table rows', () => {
       cy.visit('https://parabank.parasoft.com/parabank/services.htm');
+      
+      cy.get('span').should('be.visible');
+      cy.get('td').should('be.visible');
+      
+      // Find the table on the page, count its rows (tr), and store the count as an alias ('rowCount')
       cy.get('table').find('tr').its('length').as('rowCount');
+      
+      // Retrieve the count of rows from the alias and log it
       cy.get('@rowCount').then((count) => {
-          cy.log(`Faqja ka ${count} rresha!`);
+          cy.log(`The page has ${count} rows!`);
       });
   });
 });
